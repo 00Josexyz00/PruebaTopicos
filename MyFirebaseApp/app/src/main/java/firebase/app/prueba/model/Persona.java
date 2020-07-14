c1227f10d5e80f344ece6f2091508ea558b46b92
00Josexyz00 <jose.cortez@epn.edu.ec>
2020-07-14 13:50:44 -0500

    Crud Firebase

---
+++ MyFirebaseApp/app/src/main/java/firebase/app/prueba/model/Persona.java
@@ -0,0 +1,58 @@
+package firebase.app.prueba.model;
+
+public class Persona {
+
+    private String Uid;
+    private String Nombre;
+    private String Apellidos;
+    private String Correo;
+    private String Password;
+
+    public Persona() {
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+}
